446b17a592f65ae16d10b05eaff36191c31f51e1
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-02-11 14:49:32 -0800

    mouse over event created

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
-// Your code goes here
+
+
+//Create mouseover eventLisentener for navigation 
+
+let navigationOn = document.querySelector(".nav-link")
+    navigationOn.addEventListener('mouseover', () => {
+    navigationOn.style.backgroundColor = "teal"
+    navigationOn.style.textDecoration = "Underline"
+})
+
+let navigationOff = document.querySelector(".nav-link")
+navigationOff.addEventListener('mouseout', () => {
+    navigationOff.style.backgroundColor = ""
+    navigationOff.style.textDecoration = ""
+})
+
+
+// Create mouseover eventListener
+ let img = document.getElementsByTagName('img')
+ console.log(img)
+
+
+//Create dblclick eventLisentener
+
+// let changeImg = document.querySelector('.bus in the sand')
+// changeImg.addEventListener('dblclick', () => {
+//     changeImg.src = ""
+// })
+
+
+
